c45529ee6faf49c424a1f0ab27cdbd275a7679ec
davidbsp <davidbsportugal@sapo.pt>
2019-05-07 18:26:51 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,46 @@
+# flir_ax8_simple_driver
+
+### ver0.1 David Portugal
+Simple driver for the FLIR AX8 Thermal Camera using OpenCV and RTSP to stream the camera's image, and publishing it into a sensor_msgs/Image topic on ROS
+Tested on Ubuntu 18.04 and ROS Melodic
+
+## Compling
+
+```
+cd your_work_space
+catkin_make 
+```
+
+## Example Usage
+
+### flir_ax8_simple_driver
+
+**Parameters**
+
+`flir_camera_ip` (`string`, `default: 169.254.64.1`)
+
+By default, the IP address of the device is 169.254.64.1.
+
+`flir_camera_frame` (`string`, `default: flir_ax8`)
+
+The frame ID entry for the messages.
+
+`flir_camera_topic` (`string`, `default: camera/flir_ax8`)
+
+The topic to publish the messages.
+
+`flir_encoding` (`string`, `default: mpeg4`)
+
+The encoding type for the streamed images. Possible options are: avc, mpeg4 and mjpg.
+
+`flir_text_overlay` (`string`, `default: off`)
+
+Option to remove the overlaid text from FLIR. Possible options are: avc, mpeg4 and mjpg.
+
+**Node**
+
+```
+roslaunch flir_ax8_simple_driver flir_ax8_simple_driver.launch
+```
+
+Feel free to play with the parameters on the launch file to suit your setup.
---
+++ src/flir_ax8_simple_driver.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python2
+import cv2
+import rospy
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge, CvBridgeError
+
+def flir_publisher():
+    rospy.init_node('flir_ax8_simple_driver', anonymous=True)
+
+    camera_frame = rospy.get_param('~flir_camera_frame', "flir_ax8")
+    camera_topic = rospy.get_param('~flir_camera_topic', "camera/flir_ax8")
+    camera_ip = rospy.get_param('~flir_camera_ip', "169.254.64.1")
+    encoding = rospy.get_param('~flir_encoding', "mpeg4")       #options: avc, mpeg4 and mjpg
+    overlay = rospy.get_param('~flir_text_overlay', "off")      #options: on/off (string)
+
+    vcap = cv2.VideoCapture("rtsp://"+camera_ip+"/"+encoding+"?overlay="+overlay)
+    bridge = CvBridge()
+
+    pub = rospy.Publisher(camera_topic, Image, queue_size=10)
+    rate = rospy.Rate(10) #the actual FPS will be 8.8/9Hz
+    start_publish = True
+
+    while not rospy.is_shutdown():
+
+        #OpenCV stuff:
+        ret, frame = vcap.read()
+   	    #cv2.imshow('Flir AX8 Feed', frame)
+        #cv2.waitKey(1)
+
+        #ROS stuff:
+        pub_img = bridge.cv2_to_imgmsg(frame, encoding="passthrough")
+        pub_img.header.frame_id = camera_frame
+        pub_img.header.stamp = rospy.Time.now()
+        pub.publish(pub_img)
+
+        if start_publish:
+            rospy.loginfo("Publishing images on topic \"" + camera_topic + "\"")
+            start_publish=False
+
+        rate.sleep()
+
+if __name__ == '__main__':
+    try:
+        flir_publisher()
+    except rospy.ROSInterruptException:
+        pass
